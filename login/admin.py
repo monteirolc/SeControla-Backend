04de3618ff2669d30c13d916833b1e4db8154e73
Lucas M.C. <lucasm.castro47@gmail.com>
2025-08-21 23:02:46 -0300

    create admin.py to use the my custom login

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("login", "nome", "sobrenome", "email", "cpf", "telefone",
+                    "idade", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("login", "password")}),
+        ("Informações Pessoais", {"fields": ("nome", "sobrenome", "idade",
+                                             "cpf", "email", "telefone")}),
+        ("Permissões", {"fields": ("is_staff", "is_active", "is_superuser",
+                                   "groups", "user_permissions")}),
+        ("Datas importantes", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("login", "nome", "sobrenome", "idade", "cpf", "email",
+                       "telefone", "password1", "password2", "is_staff",
+                       "is_active")}
+         ),
+    )
+
+    search_fields = ("login", "email", "cpf", "nome", "sobrenome")
+    ordering = ("login",)

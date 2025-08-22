7dbeb398f02f6b7a1990bb9e9d303e44857d3ee7
Lucas M.C. <lucasm.castro47@gmail.com>
2025-08-21 22:59:29 -0300

    create views to login and create routes to login page

--- login/views.py
+++ login/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import CustomUser
+from .serializers import UserRegisterSerializer, UserLoginSerializer
 
-# Create your views here.
+
+class UserRegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserRegisterSerializer
+
+
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        })

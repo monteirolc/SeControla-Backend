bdd87136f45a08efe9ca4f61bb2118a9aab7ae55
Lucas M.C. <lucasm.castro47@gmail.com>
2025-08-25 10:52:25 -0300

    change in serializer name login to username

--- login/serializers.py
+++ login/serializers.py
@@ -9,7 +9,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'name', 'last_name', 'age', 'cpf', 'email', 'phone',
-                  'login', 'password']
+                  'username', 'password']
 
     def create(self, validated_data):
         password = validated_data.pop('password')
@@ -20,11 +20,11 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
 
 class UserLoginSerializer(serializers.Serializer):
-    login = serializers.CharField()
+    username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
-        user = authenticate(login=data.get('login'),
+        user = authenticate(username=data.get('username'),
                             password=data.get('password'))
         if not user:
             raise serializers.ValidationError("Invalid login credentials")

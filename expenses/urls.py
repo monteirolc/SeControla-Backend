a2bac22578923101203019ecbb1513e22991dfbc
Lucas M.C. <lucasm.castro47@gmail.com>
2025-09-09 20:27:27 -0300

    fixed import to expense viewset

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ExpenseSerializer
+from .views import ExpenseViewSet
 
 router = DefaultRouter()
-router.register(r'', ExpenseSerializer, basename='expense')
+router.register(r'', ExpenseViewSet, basename='expense')
 
 urlpatterns = [
     path('', include(router.urls)),

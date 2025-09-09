f8b1f2b0c919980216ea8d9350562349af456a2c
Lucas M.C. <lucasm.castro47@gmail.com>
2025-09-09 11:46:18 -0300

    changed urls of expenses to /

--- expenses/urls.py
+++ expenses/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ExpenseSerializer
 
 router = DefaultRouter()
-router.register(r'incomes', ExpenseSerializer, basename='income')
+router.register(r'', ExpenseSerializer, basename='expense')
 
 urlpatterns = [
     path('', include(router.urls)),

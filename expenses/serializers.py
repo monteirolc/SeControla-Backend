ea7067f5a583a08d42eeac780a1ced4746a27c65
Lucas M.C. <lucasm.castro47@gmail.com>
2025-09-19 11:18:26 -0300

    created is_valid to serializer

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -6,4 +6,18 @@ class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expense
         fields = "__all__"
-        read_only_fields = ["id", "created_at", "balance"]
+        read_only_fields = ["created_by", "updated_at", "created_at"]
+
+    def is_valid(self, raise_exception=False):
+        valid = super().is_valid(raise_exception=False)
+
+        if not valid:
+            # Aqui você pode logar ou printar os erros
+            print("⚠️ Erros de validação no ExpenseSerializer:")
+            print(self.errors)
+
+            # Ou, se quiser, levantar uma exceção mais descritiva
+            if raise_exception:
+                raise serializers.ValidationError(self.errors)
+
+        return valid

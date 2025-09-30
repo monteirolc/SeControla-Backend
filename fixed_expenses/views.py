1be3953d13f3ec9a986caa7f6e5a32b42ba70664
Lucas M.C. <lucasm.castro47@gmail.com>
2025-09-30 15:25:52 -0300

    Added update function into fixed_expenses views

--- fixed_expenses/views.py
+++ fixed_expenses/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from .serializers import FixedExpenseSerializer, FixedExpenseInstanceSerializer
 from .models import FixedExpense, FixedExpenseInstance
 from balance.models import Balance
+from rest_framework.response import Response
 
 
 class FixedExpenseViewSet(viewsets.ModelViewSet):
@@ -45,6 +46,25 @@ class FixedExpenseViewSet(viewsets.ModelViewSet):
             created_by=user,
             balance=balance
         )
+
+    def perform_update(self, serializer):
+        balance_id = self.request.data.get("balance")  # type: ignore
+        balance = Balance.objects.get(id=balance_id)
+        serializer.save(created_by=self.request.user, balance=balance)
+
+    def update(self, request, *args, **kwargs):
+        # AQUI, você busca o objeto e passa o campo que falta
+        instance = self.get_object()
+        data = request.data.copy()
+        data['date'] = (
+            f'{data['date'][6:10]}-{data['date'][3:5]}-{data['date'][0:2]}')
+        # Agora, chame o serializer com os dados completos e o 'partial'
+        serializer = self.get_serializer(instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(data=serializer.data,
+                        status=status.HTTP_200_OK)
 
 
 class FixedExpenseInstanceViewSet(viewsets.ModelViewSet):
